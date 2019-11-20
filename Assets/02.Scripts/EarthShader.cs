323b6c5651706787a8f1d23db5e212a53f7866d8
Myeong <juharim@naver.com>
2019-11-20 17:41:19 +0900

    완성(1~5)
    1. 우주공간 표현(Skybox)
    2. 3D 모델과 텍스쳐로 지구와 달을 표현
    3. 달의 공전 애니메이션 표현
    4. 달과 지구의 자전 애니메이션 표현
    5. 셰이더를 이용해 지구의 낮과 밤 애니메이션 반복적으로 표현
    + Sample Image 추가

---
+++ Assets/02.Scripts/EarthShader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthShader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float val = Mathf.PingPong(Time.time, 0.8f);
+        GetComponent<Renderer>().material.SetFloat("_TexValue", val);
+    }
+}
---
+++ Assets/02.Scripts/MoonRevolve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRevolve : MonoBehaviour
+{
+    [SerializeField] GameObject earth;
+
+    float speed = 1f;
+    float radius = 5f;
+
+    Vector3 centre;
+    float angle;
+    
+    void Start()
+    {
+        centre = earth.transform.position;
+    }
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+        var offset = new Vector3(Mathf.Sin(angle), Mathf.Tan(0), Mathf.Cos(angle)) * radius;
+        transform.position = centre + offset;
+    }
+}
